e8dfcef141eebf6c708abdd073d108d13d7d3b69
punishermortal <51474998+punishermortal@users.noreply.github.com>
2024-09-19 16:40:26 +0530

    Time: 34 ms (80.1%), Space: 16.8 MB (27.58%) - LeetHub

--- 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.py
+++ 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.py
@@ -16,5 +16,31 @@ class Solution:
         if(len(res) == 0):
             return [int(s)]
         return res
+    def solve(self, s: str, memo: dict) -> List[int]:
+        if s in memo:
+            return memo[s]
+        
+        res = []
+        for i in range(len(s)):
+            if s[i] in '+-*':
+                left_arr = self.solve(s[:i], memo)
+                right_arr = self.solve(s[i+1:], memo)
+                for x in left_arr:
+                    for y in right_arr:
+                        if s[i] == '+':
+                            res.append(x + y)
+                        elif s[i] == '-':
+                            res.append(x - y)
+                        elif s[i] == '*':
+                            res.append(x * y)
+        if not res:
+            res = [int(s)]
+        memo[s] = res
+        return res
+
+
     def diffWaysToCompute(self, expression: str) -> List[int]:
-        return self.solve(expression)
+        # return self.solve(expression)
+        memo = {}
+        return self.solve(expression, memo)
+
