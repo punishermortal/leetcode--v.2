c8a81b039728bf8dacc29e1e05dafb0f82530c10
punishermortal <51474998+punishermortal@users.noreply.github.com>
2024-10-07 10:27:45 +0530

    Time: 30 ms (99.17%), Space: 16.6 MB (7.81%) - LeetHub

--- 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.py
+++ 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.py
@@ -1,11 +1,27 @@
 class Solution:
-    def minLength(self, s: str) -> int:
-        i,count=0,0
-        while(i<len(s)-1):
-            if s[i:].startswith("AB") or s[i:].startswith("CD"):
-                s = s[:i]+s[i+2:]
-                count+=1
-                if(i>0):i-=1
-            else:
-                i+=1
-        return len(s)
+    # def minLength(self, s: str) -> int:
+    #     i,count=0,0
+    #     while(i<len(s)-1):
+    #         if s[i:].startswith("AB") or s[i:].startswith("CD"):
+    #             s = s[:i]+s[i+2:]
+    #             count+=1
+    #             if(i>0):i-=1
+    #         else:
+    #             i+=1
+    #     return len(s)
+
+
+
+        def minLength(self, s: str) -> int:
+            stack = []
+            for c in s:
+                if not stack:
+                    stack.append(c)
+                    continue
+                if c == "B" and stack[-1] == "A":
+                    stack.pop()
+                elif c == "D" and stack[-1] == "C":
+                    stack.pop()
+                else:
+                    stack.append(c)
+            return len(stack)
