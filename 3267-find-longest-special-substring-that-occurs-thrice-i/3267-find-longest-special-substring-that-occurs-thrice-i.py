f85c242de67b66026778f13b77852e8a6aae0b19
punishermortal <51474998+punishermortal@users.noreply.github.com>
2024-12-10 22:22:12 +0530

    Time: 137 ms (10.27%), Space: 17.4 MB (5.52%) - LeetHub

---
+++ 3267-find-longest-special-substring-that-occurs-thrice-i/3267-find-longest-special-substring-that-occurs-thrice-i.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maximumLength(self, s: str) -> int:
+        hsh={}
+        for i in range(len(s)):
+            for j in range(i+1,len(s)+1):
+                if s[i:j] in hsh.keys() :
+                    hsh[s[i:j]]+=1
+                else:
+                    hsh[s[i:j]]=1
+        mx=-1
+        res=[]
+        for i in hsh.keys():
+            if hsh[i]>=3 and len(i)>mx and len(set(i))==1:
+                res.append(i)
+                mx=len(i)
+        return(mx)
