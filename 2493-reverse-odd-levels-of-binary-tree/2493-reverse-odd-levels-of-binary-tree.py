f745ed4c59c00e44cf6b2388ba1d4b66788a76c4
punishermortal <51474998+punishermortal@users.noreply.github.com>
2024-12-21 09:52:31 +0530

    Time: 107 ms (6.02%), Space: 23.1 MB (6.41%) - LeetHub

--- 2493-reverse-odd-levels-of-binary-tree/2493-reverse-odd-levels-of-binary-tree.py
+++ 2493-reverse-odd-levels-of-binary-tree/2493-reverse-odd-levels-of-binary-tree.py
@@ -1,37 +1,37 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def reverseOddLevels(self, root):
-        if not root:
-            return None  # Return None if the tree is empty.
+    def reverseNodeValue(self,arr:List[Optional[TreeNode]]) -> None:
+        left,right = 0,len(arr)-1
+        while(left < right):
+            temp = arr[left].val
+            arr[left].val = arr[right].val
+            arr[right].val = temp
+            left += 1
+            right -= 1
 
-        queue = [root]  # Start BFS with the root node.
+    def reverseOddLevels(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if root == None or (root.left == None and root.right == None):
+            return root
+        qu = [root]
         level = 0
-
-        while queue:
-            size = len(queue)
-            current_level_nodes = []
-
-            # Process all nodes at the current level.
-            for _ in range(size):
-                node = queue.pop(0)
-                current_level_nodes.append(node)
-
-                if node.left:
-                    queue.append(node.left)
-                if node.right:
-                    queue.append(node.right)
-
-            # Reverse node values if the current level is odd.
-            if level % 2 == 1:
-                left, right = 0, len(current_level_nodes) - 1
-                while left < right:
-                    tmp = current_level_nodes[left].val
-                    current_level_nodes[left].val = current_level_nodes[
-                        right
-                    ].val
-                    current_level_nodes[right].val = tmp
-                    left += 1
-                    right -= 1
-
+        while(len(qu)>0):
+            curr_level_node_store =[]
+            length_of_qu = len(qu)
+            for i in range(length_of_qu):
+                curr_node_pop =qu.pop(0)
+                curr_level_node_store.append(curr_node_pop)
+                if curr_node_pop.left is not None:
+                    qu.append(curr_node_pop.left)
+                if curr_node_pop.right is not None:
+                    qu.append(curr_node_pop.right)
+            if level %2 == 1:
+                # reverse the array only nbode's value
+                self.reverseNodeValue(curr_level_node_store)
             level += 1
-
-        return root  # Return the modified tree root.
+        return root
+        
